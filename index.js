6439fb92224338b97fdbbc4284f7ceeda0f79231
Anusha806 <anushamajji806@gmail.com>
2024-05-18 18:41:04 +0530

    guesswordcommit

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ function memoryGame(){
     window.open("memoryGame.html", "_blank");
 }
 
-
+CSS.registerProperty({
+    name: "--gradientColor1",
+    syntax: '<color>',
+    inherits: true,
+    initialValue: "#09ec3a"
+  });
+  
+  CSS.registerProperty({
+    name: "--gradientColor2",
+    syntax: '<color>',
+    inherits: true,
+    initialValue: "#040c27"
+  });
+  
+  CSS.registerProperty({
+    name: "--gradientColor3",
+    syntax: '<color>',
+    inherits: true,
+    initialValue: "#37a5ee"
+  });
+  
